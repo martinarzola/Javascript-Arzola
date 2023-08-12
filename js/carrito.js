eef25c855762f562f22ce50225d711ce896f27c3
Martín Arzola <martin.arzola.olivares@gmail.com>
2023-08-11 22:47:04 -0400

    Cambios 2

--- js/carrito.js
+++ js/carrito.js
@@ -1,1 +1,83 @@
- 
+// Función para calcular el subtotal
+function calcularSubtotal(productos) {
+  return productos.reduce((total, product) => total + product.precio * product.cantidad, 0);
+}
+
+// Obtener el array de productos desde localStorage
+let productosTotales = JSON.parse(localStorage.getItem("cart")) || [];
+
+// Filtrar productos con cantidad distinta de cero
+const productosConCantidad = productosTotales.filter((product) => product.cantidad !== 0);
+
+// Función para crear un div de producto
+function createProductDiv(product) {
+  const productDiv = document.createElement("div");
+  productDiv.className = "product";
+  productDiv.innerHTML = `
+    <div class="product-info">
+      <div class="column"> <img src="${product.imagen}" alt="${product.nombre}" class="product-image"></div>
+      <div class="column">${product.nombre}
+      $${product.precio}</div>
+      <div class="column">${product.cantidad}</div>
+      <div class="column">
+        <button class="btn-sumar" data-id="${product.id}">+</button>
+        <button class="btn-restar" data-id="${product.id}">-</button>
+      </div>
+    </div>
+  `;
+  
+  // Agregar eventos a los botones de sumar y restar
+  const btnSumar = productDiv.querySelector(".btn-sumar");
+  const btnRestar = productDiv.querySelector(".btn-restar");
+  
+  btnSumar.addEventListener("click", () => {
+    product.cantidad += 1;
+    guardarEnLocalStorage(productosTotales);
+    actualizarProductos();
+    actualizarSubtotal(productosTotales);
+  });
+
+
+  btnRestar.addEventListener("click", () => {
+    if (product.cantidad > 0) {
+      product.cantidad -= 1;
+      guardarEnLocalStorage(productosTotales);
+      actualizarProductos();
+      actualizarSubtotal(productosTotales);
+    }
+  });
+
+  return productDiv;
+}
+
+// Función para actualizar el subtotal en el DOM
+function actualizarSubtotal(productos) {
+  const subtotalElement = document.getElementById("subtotal");
+  const subtotal = calcularSubtotal(productos);
+  subtotalElement.innerHTML = `<h4>Subtotal: $${subtotal}</h4>`;
+}
+
+// Función para actualizar la sección de productos en el DOM
+function actualizarProductos() {
+  const productsContainer = document.getElementById("carrito-contenido");
+  productsContainer.innerHTML = "";
+  
+  productosConCantidad.forEach((product) => {
+    const productDiv = createProductDiv(product);
+    productsContainer.appendChild(productDiv);
+  });
+}
+
+// Botón para vaciar localStorage y limpiar el DOM
+const btnVaciar = document.getElementById("btn-vaciar");
+btnVaciar.addEventListener("click", () => {
+  localStorage.clear();
+  productosConCantidad.length = 0;
+  actualizarProductos();
+  actualizarSubtotal(productosConCantidad);
+  alert("Se ha vaciado el carrito.");
+});
+
+// Llamada inicial para cargar los productos en el DOM
+actualizarProductos();
+actualizarSubtotal(productosConCantidad);
--- js/main.js
+++ js/main.js
@@ -1,135 +1,43 @@
 const productos = [
-  {
-    id: 1,
-    nombre: "Leche entera",
-    precio: 8,
-    cantidad: 0,
-  },
-  {
-    id: 2,
-    nombre: "Leche Semi-Descremada",
-    precio: 10,
-    cantidad: 0,
-  },
-  {
-    id: 3,
-    nombre: "Leche descremada",
-    precio: 12,
-    cantidad: 0,
-  },
-  {
-    id: 4,
-    nombre: "Queso mantecoso",
-    precio: 12,
-    cantidad: 0,
-  },
-  {
-    id: 5,
-    nombre: "Queso gauda",
-    precio: 14,
-    cantidad: 0,
-  },
-  {
-    id: 6,
-    nombre: "Queso mozzarella",
-    precio: 15,
-    cantidad: 0,
-  },
-  {
-    id: 7,
-    nombre: "Mantequilla Con Sal",
-    precio: 11,
-    cantidad: 0,
-  },
-  {
-    id: 8,
-    nombre: "Mantequilla Sin Sal",
-    precio: 13,
-    cantidad: 0,
-  },
-  {
-    id: 9,
-    nombre: "Dulce de leche",
-    precio: 10,
-    cantidad: 0,
-  },
-  {
-    id: 10,
-    nombre: "Yogur",
-    precio: 6,
-    cantidad: 0,
-  },
+  { id: 1, nombre: "Leche entera", precio: 8, cantidad: 0, imagen: "../images/leche-entera.png" },
+  { id: 2, nombre: "Leche semidescremada", precio: 10, cantidad: 0, imagen: "../images/leche-semi.png" },
+  { id: 3, nombre: "Leche descremada", precio: 12, cantidad: 0, imagen: "../images/leche-des.png" },
+  { id: 4, nombre: "Queso mantecoso", precio: 12, cantidad: 0, imagen: "../images/queso-mant.png" },
+  { id: 5, nombre: "Queso gauda", precio: 14, cantidad: 0, imagen: "../images/queso-gaud.png" },
+  { id: 6, nombre: "Queso mozzarella", precio: 15, cantidad: 0, imagen: "../images/queso-mozz.png" },
+  { id: 7, nombre: "Mantequilla Con Sal", precio: 11, cantidad: 0, imagen: "../images/mantequilla-sal.png" },
+  { id: 8, nombre: "Mantequilla Sin Sal", precio: 13, cantidad: 0, imagen: "../images/mantequilla-no-sal.png" },
+  { id: 9, nombre: "Dulce de leche", precio: 10, cantidad: 0, imagen: "../images/dulce-de-leche.png" },
+  { id: 10, nombre: "Yogur", precio: 6, cantidad: 0, imagen: "../images/yogurt-natural.png" },
 ];
 
-// Función para añadir un producto al carrito y almacenarlo en localStorage
-function agregarAlCarrito(id) {
-  const producto = productos.find((prod) => prod.id === id);
-  producto.cantidad++;
-  actualizarCarrito();
-  guardarEnLocalStorage();
-}
+// Función para manejar el evento de clic en los botones "Añadir al carrito"
+function addToCart(e) {
+  const productId = parseInt(e.target.getAttribute("data-id")); // Obtener el ID del producto desde el atributo data-id
+  const product = productos.find((p) => p.id === productId); // Buscar el producto en el array de productos
+  if (product) {
+    product.cantidad += 1;
+    guardarEnLocalStorage(productos);
 
-// Función para guardar el contenido del carrito en localStorage
-function guardarEnLocalStorage() {
-  localStorage.setItem("carrito", JSON.stringify(productos));
-}
-
-// Función para cargar el contenido del carrito desde localStorage
-function cargarDesdeLocalStorage() {
-  const carritoGuardado = localStorage.getItem("carrito");
-  if (carritoGuardado) {
-    const productosGuardados = JSON.parse(carritoGuardado);
-    for (const producto of productosGuardados) {
-      const productoActual = productos.find((prod) => prod.id === producto.id);
-      if (productoActual) {
-        productoActual.cantidad = producto.cantidad;
-      }
-    }
+    alert(`El producto ${product.nombre} se ha añadido al carrito`)
   }
 }
 
-// Función para actualizar el contenido del carrito.html
-function actualizarCarrito() {
-  const carritoDiv = document.getElementById("carrito-contenido");
-  carritoDiv.innerHTML = "";
-
-  productos.forEach((producto) => {
-    if (producto.cantidad > 0) {
-      const productoDiv = document.createElement("div");
-      productoDiv.classList.add("producto-carrito");
-
-      const imagen = document.createElement("img");
-      imagen.src = producto.imagen;
-      imagen.alt = producto.nombre;
-      productoDiv.appendChild(imagen);
-
-      const nombre = document.createElement("h2");
-      nombre.textContent = producto.nombre;
-      productoDiv.appendChild(nombre);
-
-      const precio = document.createElement("p");
-      precio.textContent = `$${producto.precio}`;
-      productoDiv.appendChild(precio);
-
-      const cantidad = document.createElement("p");
-      cantidad.textContent = `Cantidad: ${producto.cantidad}`;
-      productoDiv.appendChild(cantidad);
-
-      carritoDiv.appendChild(productoDiv);
-    }
-  });
+// Función para guardar el array de productos en localStorage
+function guardarEnLocalStorage(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-// Cargamos el contenido del carrito desde localStorage al cargar la página
-window.addEventListener("load", () => {
-  cargarDesdeLocalStorage();
-  actualizarCarrito();
-});
+const buttons = document.getElementsByClassName("btn-anadir-carrito");
+for (const button of buttons) {
+  button.addEventListener("click", addToCart);
+  const productId = parseInt(button.getAttribute("data-id")); // Obtener el ID del producto desde el atributo data-id
+  button.setAttribute("data-id", productId); // Actualizar el atributo data-id con el ID numérico
+}
 
-// Asignamos el evento de clic a los botones "Añadir al carrito"
-const botonesAgregar = document.querySelectorAll(".btn-anadir-carrito");
-botonesAgregar.forEach((boton, index) => {
-  boton.addEventListener("click", () => {
-    agregarAlCarrito(index + 1);
-  });
-});
+// Formulario para contacto
+let botonFormContact = document.getElementById("enviarBtn");
+botonFormContact.onclick = () =>
+  alert(
+    "¡Gracias por sus comentarios! Recibirá una respuesta a su correo electrónico."
+  );
